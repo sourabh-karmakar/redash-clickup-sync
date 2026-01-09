e0cc59f7b2129e5f0659307747e3508894829b55
sourabh-karmakar <sourabh.karmakar@snapmint.com>
2026-01-09 16:30:03 +0530

    Update sync.py

--- scripts/sync.py
+++ scripts/sync.py
@@ -1,31 +1,30 @@
 import requests
 import os
 
-REDASH_URL = os.environ["https://reports.snapmint.com"]
-REDASH_API_KEY = os.environ["xbV5CDo1VQCNvmaeSs7tUNTWYLfeoVsRDAMuOQI6"]
-QUERY_ID = os.environ["14581"]
+REDASH_URL = os.environ["REDASH_URL"]
+REDASH_API_KEY = os.environ["REDASH_API_KEY"]
+QUERY_ID = os.environ["QUERY_ID"]
 
-CLICKUP_TOKEN = os.environ["9I9UBYDQ12D8XEJXW0NSSCVE0SXLDFZA879H9PZ9XDA7QS6XFV8R79HE8X032AI1"]
-CLICKUP_LIST_ID = os.environ["901412176234"]
+CLICKUP_TOKEN = os.environ["CLICKUP_TOKEN"]
+CLICKUP_LIST_ID = os.environ["CLICKUP_LIST_ID"]
 
 # Fetch Redash data
-redash_api = f"https://reports.snapmint.com/api/queries/14581/results.json?api_key=xbV5CDo1VQCNvmaeSs7tUNTWYLfeoVsRDAMuOQI6"
+redash_api = f"{REDASH_URL}/api/queries/{QUERY_ID}/results.json?api_key={REDASH_API_KEY}"
 response = requests.get(redash_api)
 data = response.json()["query_result"]["data"]["rows"]
 
 # Example: Create one ClickUp task per row
 for row in data:
     task_payload = {
-        "name": f"Metric Alert: {row['Add Unleash MID']}",
-        "description": f"Value: {row['id']}",
+        "name": f"Metric Alert: {row['metric_name']}",
+        "description": f"Value: {row['metric_value']}",
         "status": "to do"
     }
 
-    clickup_url = f"https://api.clickup.com"
+    clickup_url = f"https://api.clickup.com/api/v2/list/{CLICKUP_LIST_ID}/task"
     headers = {
         "Authorization": CLICKUP_TOKEN,
         "Content-Type": "application/json"
     }
 
     requests.post(clickup_url, json=task_payload, headers=headers)
-
